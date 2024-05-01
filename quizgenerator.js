a6275720f2d9fe59fc0fd00f5bf93ceeac45112e
Mehmet Ateş Özateş <ozatesatess@gmail.com>
2024-05-01 03:32:44 +0300

    Quiz questions modified

--- quizgenerator.js
+++ quizgenerator.js
@@ -13,12 +13,12 @@ Question.prototype.checkAnswer = function (answer) {
 
 
 let questions = [
-    new Question("1)Hangisi javascript paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm", d: "Angular.js" }, "c"),
-    new Question("2)Hangisi .net paket yönetim uygulamasıdır?", { a: "Node.js", b: "Nuget", c: "Npm", d: "Angular.js" }, "b"),
-    new Question("3)Hangisi xamarin paket yönetim uygulamasıdır?", { a: "Node.js", b: "Nuget", c: "Npm", d: "Angular.js" }, "b"),
-    new Question("4)Hangisi django paket yönetim uygulamasıdır?", { a: "Node.js", b: "Nuget", c: "Npm", d: "Angular.js" }, "b"),
-    new Question("5)Hangisi .Net paket yönetim uygulamasıdır?", { a: "Node.js", b: "Nuget", c: "Npm", d: "Angular.js" }, "b"),
-    new Question("6)Hangisi .Net paket yönetim uygulamasıdır?", { a: "Node.js", b: "Nuget", c: "Npm", d: "Angular.js" }, "b"),
+    new Question("1)Aşağıdakilerden hangisi javascript paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm", d: "Angular.js" }, "c"),
+    new Question("2)Aşağıdakilerden hangisi .net paket yönetim uygulamasıdır?", { a: "Node.js", b: "Nuget", c: "Npm", d: "Angular.js" }, "b"),
+    new Question("3)Aşağıdakilerden hangisi bir programlama dili değildir?", { a: "C#", b: "Java", c: "Python", d: "React.js" }, "d"),
+    new Question("4)Aşağidakilerden hangisi .net geliştirmede kullanılan ide'dir?", { a: "Eclipse", b: "Pycharm", c: "Visual Studio", d: "Phpstorm" }, "c"),
+    new Question("5)Aşağıdakilerden hangisi mobil geliştirmede kullanılan bir teknoloji değildir?", { a: "React Native", b: "Oracle", c: "Flutter", d: "Kotlin" }, "b"),
+    new Question("6)Aşağıdakilerden hangisi bir versiyon kontrol sistemidir? ", { a: "Git", b: "Redux", c: "Docker", d: "Asp.net" }, "a"),
 
 ];
 
