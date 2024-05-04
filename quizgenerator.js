1308d75a2b8f3a111680167ebcf04741c9bffeb3
Mehmet Ateş Özateş <ozatesatess@gmail.com>
2024-05-05 02:06:02 +0300

    Refactorings of the project  done

--- quizgenerator.js
+++ quizgenerator.js
@@ -5,31 +5,57 @@ const ui = new UI();
 ui.btn_start.addEventListener("click", function() {
     
      ui.quiz_box.classList.add("active");
+     startTimer(10);
+     startTimerLine();
      ui.showQuestion(quiz.getQuestion());
      ui.showTheNumberOfQuestions(quiz.questionIndex + 1,quiz.questions.length);
      ui.btn_next.classList.remove("show");
 })
 
 ui.btn_next.addEventListener("click",function() {
-    if (quiz.questions.length != quiz.questionIndex + 1) 
+    if(quiz.questions.length != quiz.questionIndex + 1) 
     {
         quiz.questionIndex += 1;
+        clearInterval(counter);
+        clearInterval(counterLine);
+        ui.time_text.textContent = "Kalan Süre";
+        ui.time_second.textContent = 10;
+        startTimer(10);
+        startTimerLine();
         ui.showQuestion(quiz.getQuestion());
         ui.showTheNumberOfQuestions(quiz.questionIndex + 1,quiz.questions.length);
         ui.btn_next.classList.remove("show");
         
-    } else {
-        console.log("quiz tamamlandı.");
+    }else{
+        clearInterval(counter);
+        clearInterval(counterLine);
+        ui.quiz_box.classList.remove("active");
+        ui.score_box.classList.add("active");
+        ui.showScore(quiz.questions.length,quiz.numberofCorrectAnswers);
     }
 });
 
-   function optionSelected(option) {
+ui.btn_quit.addEventListener("click",function(){
+    window.location.reload(); 
+});
+
+ui.btn_restart.addEventListener("click",function(){
+    quiz.questionIndex = 0;
+    quiz.numberofCorrectAnswers = 0;
+    ui.btn_start.click();
+    ui.score_box.classList.remove("active"); 
+});
+  
+
+function optionSelected(option) {
+        clearInterval(counter);
         let answer = option.querySelector("span b").textContent;
         let question = quiz.getQuestion();
     
 
     if(question.checkAnswer(answer)) 
-    {
+    { 
+        quiz.numberofCorrectAnswers +=1;
         option.classList.add("correct");
         option.insertAdjacentHTML("beforeend",ui.correctIcon);
     }else{
@@ -43,6 +69,56 @@ ui.btn_next.addEventListener("click",function() {
     }
 
     ui.btn_next.classList.add("show");
+
+}
+
+let counter;
+
+function startTimer(time)
+{
+    counter = setInterval(timer,1000);
+
+    function timer(){
+        ui.time_second.textContent = time;
+        time--; 
+
+        if(time < 0)
+        {
+           clearInterval(counter);
+
+           ui.time_text.textContent = "Süre Bitti";
+           let answer =  quiz.getQuestion().correctAnswer; 
+           for(let option of ui.option_list.children)
+           {
+               if(option.querySelector("span b").textContent == answer) 
+               {
+                 option.classList.add("correct");
+                 option.insertAdjacentHTML("beforeend",ui.correctIcon);
+               }
+               option.classList.add("disabled");
+            }
+            ui.btn_next.classList.add("show");
+        }
+    }
+}
+
+let counterLine;
+
+function startTimerLine()
+{
+    let line_width = 0;
+
+   counterLine= setInterval(timer,20);
+
+    function timer(){
+        line_width += 1; 
+        ui.time_line.style.width = line_width + "px";
+
+        if(line_width>549)
+        {
+            clearInterval(counterLine);
+        }
+    }
 
 }
 
--- ui.js
+++ ui.js
@@ -2,11 +2,20 @@ function UI(){
     this.btn_start = document.querySelector(".btn_start"),
     this.btn_next = document.querySelector
     (".btn_next"),
+    this.btn_restart = document.querySelector
+    (".btn_restart"),
+    this.btn_quit = document.querySelector
+    (".btn_quit"),
     this.quiz_box = document.querySelector
     (".quiz_box"),
+    this.score_box = document.querySelector
+    (".score_box"),
     this.option_list = document.querySelector(".option_list"),
     this.correctIcon = '<div class="icon"><i class="fa-solid fa-check"></i></div>',
     this.incorrectIcon = '<div class="icon"><i class="fa-solid fa-times"></i></div>';
+    this.time_text = document.querySelector(".time_text"),
+    this.time_second = document.querySelector(".time_second"),
+    this.time_line = document.querySelector(".time_line")
 }
 
 
@@ -35,4 +44,9 @@ UI.prototype.showQuestion = function showQuestion(soru) {
 UI.prototype.showTheNumberOfQuestions = function showTheNumberOfQuestions(questionOrder,totalQuestion){
     let tag = `<span class="badge bg-warning text-dark"><b>${questionOrder} / ${totalQuestion}</b></span>`;
     document.querySelector(".quiz_box .question_index").innerHTML = tag;
+}
+
+UI.prototype.showScore = function(totalQuestion,numberofCorrectAnswers){
+    let tag = `Toplam ${totalQuestion} sorudan ${numberofCorrectAnswers} doğru cevap verdiniz.`;
+    document.querySelector(".score_box .score_text").innerHTML = tag;
 }
